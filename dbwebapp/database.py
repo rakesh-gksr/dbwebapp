403a42d3cf41494671ad5c0ddfd411d56f382936
rakeshj <rakeshj@surveymonkey.com>
2019-03-21 14:34:29 +0530

    updated the database

--- dbwebapp/database.py
+++ dbwebapp/database.py
@@ -12,23 +12,19 @@ engines = {
 
 def config():
     service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    print("service_name==>", service_name)
     if service_name:
         engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
     else:
         engine = engines['sqlite']
-    print("engine:")
-    print(engine)
-    name = os.getenv('DATABASE_NAME')
+    name = os.getenv('DB_NAME')
     if not name and engine == engines['sqlite']:
         name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
     a = {
         'ENGINE': engine,
-        'NAME': os.getenv('DB_NAME'),
+        'NAME': name,
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_SERVICE_NAME'),
         'PORT': os.getenv('PORT'),
     }
-    print("a:",a)
     return a
