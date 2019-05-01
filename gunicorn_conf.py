68b0a986c86035bf35a8dd9b83b8ed6cf917a223
rakeshj <rakeshj@surveymonkey.com>
2019-05-01 16:27:03 +0530

    added new file

---
+++ gunicorn_conf.py
@@ -0,0 +1,5 @@
+import multiprocessing
+#gunicorn dbwebapp.wsgi --config gunicorn_conf.py
+bind = "0.0.0.0:8000"
+print("cpu_count",multiprocessing.cpu_count())
+workers = multiprocessing.cpu_count()
