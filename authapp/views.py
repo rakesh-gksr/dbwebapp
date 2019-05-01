9f05a42a9e0d9d0c0a6fa5765b8ea18a18a1c0f3
rakeshj <rakeshj@surveymonkey.com>
2019-05-01 14:57:27 +0530

    fgdsfgdsf

--- authapp/views.py
+++ authapp/views.py
@@ -9,5 +9,4 @@ def user_token(request):
 
     """
     response = {'test env': os.getenv("TEST_ENV", 'test')}
-    tet = {'test env': 'gdfgsdfg'}
-    return Response(tet, status=200)
+    return Response(response, status=200)
